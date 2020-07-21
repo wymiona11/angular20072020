caacd138d9ee248fe88f23589b36a8b6240eb916
wymiona11 <wymiona11@gmail.com>
2020-07-21 19:59:32 +0200

    pierwszy commit na branchu pierwsza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kurs';
+  config: { [key: string]: string | Date } = null;
+  constructor() {
+    this.config = {
+      title: 'lista zadan',
+      timeData: new Date(),
+      footer: 'to jest zajebana stopka',
+    };
+  }
 }
